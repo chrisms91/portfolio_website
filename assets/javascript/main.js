fdbcfb79647e7c1da222c8973a8a359c84556950
Christopher Kim <christopherkim@Christophers-MacBook-Pro-2.local>
2017-10-10 16:57:50 -0700

    working on it

---
+++ assets/javascript/main.js
@@ -0,0 +1,5 @@
+// var introEl = $('div.intro');
+// var introHeadingH = introEl.find('h1').height();
+// var windowH = $(window).height();
+
+// introEl.css('padding', (windowH - introHeadingH)/2 + 'px 0');
