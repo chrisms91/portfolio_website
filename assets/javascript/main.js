cb6d461ac6fbf370c983dbe0613b494c931ea500
Christopher Kim <christopherkim@Christophers-MacBook-Pro-2.local>
2017-10-11 17:51:05 -0700

    intro animation done

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,4 +11,20 @@
         offset:       100
     });
     wow.init();
+
+    // Grow and Shrink hr tag
+    var $hr = $('#intro-line');
+    setTimeout(function() {
+    	$hr.css("width", "80%");
+    }, 100);
+
+    setTimeout(function() {
+    	$hr.css("width", 0);
+    }, 1200);
+
+    // Delay fadein animation
+    setTimeout(function() {
+    	$('#intro-name').show().addClass('animated fadeInUp');
+    	$('#intro-sub').show().addClass('animated fadeInDown');
+    }, 800);
 })(jQuery);
