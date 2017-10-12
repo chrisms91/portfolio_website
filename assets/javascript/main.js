ba35d418ca81dfe743ca1d75efbf9f665da701d9
Christopher Kim <christopherkim@Christophers-MacBook-Pro-2.local>
2017-10-11 23:22:19 -0700

    added slideUp for intro page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,5 @@
-// var introEl = $('div.intro');
-// var introHeadingH = introEl.find('h1').height();
-// var windowH = $(window).height();
-
-// introEl.css('padding', (windowH - introHeadingH)/2 + 'px 0');
-
 (function ($) {
+
     // Init Wow
     wow = new WOW( {
         animateClass: 'animated',
@@ -12,19 +7,29 @@
     });
     wow.init();
 
-    // Grow and Shrink hr tag
+    //Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            // console.log(data.curTop);
+        }
+    });
+
+    // Grow hr tag
     var $hr = $('#intro-line');
     setTimeout(function() {
     	$hr.css("width", "80%");
     }, 100);
 
+    // Shrink hr tag back to 0
     setTimeout(function() {
     	$hr.css("width", 0);
     }, 1200);
 
-    // Delay fadein animation
+    // Delay fadein animation for intro page
     setTimeout(function() {
     	$('#intro-name').show().addClass('animated fadeInUp');
     	$('#intro-sub').show().addClass('animated fadeInDown');
     }, 800);
+
 })(jQuery);
